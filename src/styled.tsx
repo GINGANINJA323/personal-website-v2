3ce431ab2808c9ee944471ac0bd37fa4c3902848
Edward Glendinning <edward82009@hotmail.com>
2025-03-13 21:16:03 +0000

    feat: add optional underline styles for links

--- src/styled.tsx
+++ src/styled.tsx
@@ -5,7 +5,12 @@ export const LinedBlock = styled.div<{ underlineColour: string }>`
     border-bottom: 1px solid ${props => props.underlineColour};
 `;
 
-export const StyledLink = styled.a<{textColour: string}>`
-    text-decoration: none;
+interface StyledLinkProps {
+    textColour: string;
+    underline?: boolean;
+}
+
+export const StyledLink = styled.a<StyledLinkProps>`
+    text-decoration: ${(props) => props.underline ? 'underline' : 'none'};
     color: ${props => props.textColour};
 `;
